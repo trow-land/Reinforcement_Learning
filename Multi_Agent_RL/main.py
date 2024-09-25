1327363a41c6c64e6a5d0a1f721e17663bb12529
Tom Rowland <tom_rowland@outlook.com>
2024-09-25 17:52:42 +0100

    randomly moving agents in gridworld

--- Multi_Agent_RL/main.py
+++ Multi_Agent_RL/main.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 class GridWorld:
@@ -30,60 +31,65 @@ class GridWorld:
         print(self.grid)
 
 
-    def visualise_grid(self):
-        plt.imshow(self.grid, cmap='Pastel1', origin='upper')
-        plt.title('GridWorld')
-        plt.grid(False)
-        plt.show()
-
     def agent_step(self, agent_position, action):
         x, y = agent_position
 
         if action == 'up':
-            y -= 1
-            y = max(0, y)
-        elif action == 'down':
             y += 1
             y = min(y, self.size - 1)
+        elif action == 'down':
+            y -= 1
+            y = max(y, 0)
         elif action == 'left':
             x -= 1
-            x = max(0, x)
+            x = max(x, 0)
         elif action == 'right':
             x += 1
             x = min(x, self.size - 1)
 
-        # make the move
-        self.grid[x, y] = self.grid[agent_position]
+        if self.grid[x, y] != self.grid[agent_position[0], agent_position[1]]:
+            # make the move
+            self.grid[x, y] = self.grid[agent_position[0], agent_position[1]]
+            # reset original position to background colour
+            self.grid[agent_position] = 0
+        else:
+            # agent is at a boundary and hasnt moved
+            pass
 
-        # reset original position to background colour
-        self.grid[agent_position] = 0
-        
-        
-        
-
-
-
-
-# create a gridworld
-env = GridWorld()
-
-# add some agents
-env.add_agents((0,2), 'prey')
-env.add_agents((6,7), 'predator')
-
-env.visualise_grid()
-
-env.agent_step(env.prey[0], 'down')
-
-env.visualise_grid()
-
-env.agent_step(env.predators[0], 'up')
-env.visualise_grid()
+        return (x, y)
 
 
-# define action space
+    def visualize_grid_dynamic(self):
+        plt.clf() # clear plot
+        plt.imshow(self.grid, cmap='Pastel1', origin='upper')
+        plt.title('GridWorld')
+        plt.grid(True)
+        plt.draw()  # new plot
+        plt.pause(0.5) 
+
+# moving matplotlib
+plt.ion()
+
+# Create environment
+env = GridWorld(size=8)
+env.add_agents((1, 1), 'predator')
+env.add_agents((6, 7), 'prey')
+
 possible_actions = ['up', 'down', 'left', 'right']
 
-# implement some random actions for both predator and prey and watch them navigate the environment
+# Simulate random movement until prey is caught
 while env.prey[0] != env.predators[0]:
-    pass
+    prey_move = possible_actions[np.random.randint(0, 4)]  # Random prey move
+    predator_move = possible_actions[np.random.randint(0, 4)]  # Random predator move
+
+    # Move prey and predator
+    env.prey[0] = env.agent_step(env.prey[0], prey_move)
+    env.predators[0] = env.agent_step(env.predators[0], predator_move)
+
+    env.visualize_grid_dynamic()
+
+    #time.sleep(0.5)
+
+plt.ioff()
+plt.show()
+    
